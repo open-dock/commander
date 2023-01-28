7c0970de58498cf4d24d408d092c9eb9588cad03
mohammad esmaiel emadi <emadi68moh@gmail.com>
2023-01-28 22:44:50 +0330

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: 'standard-with-typescript',
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: 'tsconfig.json'
-  },
-  rules: {}
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'standard-with-typescript',
+		'plugin:react/recommended',
+		'airbnb',
+		'prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: 'tsconfig.json',
+	},
+	rules: {},
+};
